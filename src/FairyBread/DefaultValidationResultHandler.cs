1209d132c7165a167fae2c1ae10db35b85719f4d
Ben McCallum <ben.mccallum@live.com.au>
2020-07-26 20:05:46 +0200

    feat: New override point on error building

--- src/FairyBread/DefaultValidationResultHandler.cs
+++ src/FairyBread/DefaultValidationResultHandler.cs
@@ -28,18 +28,21 @@ namespace FairyBread
 
         protected virtual void HandleFailure(IMiddlewareContext context, ValidationFailure failure)
         {
-            var error = ErrorBuilder.New()
+            var error = ExtractError(context, failure)
+                .Build();
+
+            context.ReportError(error);
+        }
+
+        protected virtual IErrorBuilder ExtractError(IMiddlewareContext context, ValidationFailure failure)
+            => ErrorBuilder.New()
                 .SetMessage(failure.ErrorMessage)
                 .SetExtension(nameof(failure.ResourceName), failure.ResourceName)
                 .SetExtension(nameof(failure.PropertyName), failure.PropertyName)
                 .SetExtension(nameof(failure.ErrorCode), failure.ErrorCode)
                 .SetExtension(nameof(failure.Severity), failure.Severity)
                 .AddLocation(context.FieldSelection)
-                .SetPath(context.Path)
-                .Build();
-
-            context.ReportError(error);
-        }
+                .SetPath(context.Path);
 
         protected virtual void HandleValid(IMiddlewareContext context, ValidationResult result)
         {
