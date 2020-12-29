921e4ff9322f31df29140030c9954a6397522c4f
Ben McCallum <ben.mccallum@live.com.au>
2020-12-29 12:14:33 +0100

    Fix for Failures serialization

--- src/FairyBread/ValidationErrorFilter.cs
+++ src/FairyBread/ValidationErrorFilter.cs
@@ -41,7 +41,7 @@ namespace FairyBread
             return error
                 .WithMessage("Validation errors occurred.")
                 .WithCode("FairyBread_ValidationError")
-                .SetExtension("Failures", validationException.Errors.Select(FormatFailure))
+                .SetExtension("Failures", validationException.Errors.Select(FormatFailure).ToArray())
                 .RemoveException();
         }
 
