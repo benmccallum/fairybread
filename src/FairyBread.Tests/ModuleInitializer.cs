170ce72f8935b13f1b29ca009ebad089f399c085
Simon Cropp <simon.cropp@gmail.com>
2021-06-14 22:34:19 +1000

    update test refs (#49)

---
+++ src/FairyBread.Tests/ModuleInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using VerifyTests;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        VerifierSettings.ModifySerialization(settings =>
+        {
+            settings.IgnoreMember<Exception>(_ => _.StackTrace);
+        });
+    }
+}
+#if !NET5_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    sealed class ModuleInitializerAttribute : Attribute
+    {
+    }
+}
+#endif
